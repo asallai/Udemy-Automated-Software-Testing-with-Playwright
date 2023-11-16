5d99afdeeb432182e8d9b2191fd2be45a7f5f9bc
Aniko Sallai <aniko.sallai@gmail.com>
2023-11-16 22:45:40 +0100

    35. Create Page Model Class for Login

--- tests/e2e/e2e-currency-exhange.spec.ts
+++ tests/e2e/e2e-currency-exhange.spec.ts
@@ -1,6 +1,6 @@
 import {test, expect} from "@playwright/test"
 
-test.describe.only('Currency Exchange Form', () => {
+test.describe('Currency Exchange Form', () => {
     
     test.beforeEach(async ({ page }) => {
         await page.goto('http://zero.webappsecurity.com/index.html')
