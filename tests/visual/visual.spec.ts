49111f5d211ac1d9a0207e1061432ea6e733c6a5
Aniko Sallai <aniko.sallai@gmail.com>
2023-11-20 21:32:10 +0100

    48. Single Element Snapshots

--- tests/visual/visual.spec.ts
+++ tests/visual/visual.spec.ts
@@ -1,8 +1,14 @@
 import { test, expect } from "@playwright/test"
 
 test.describe("Visual Regression Test Example", () => {
-    test.only("Full Page Snapshot", async ({ page }) => {
+    test("Full Page Snapshot", async ({ page }) => {
         await page.goto("https://example.com")
         expect(await page.screenshot()).toMatchSnapshot("homepage.png")        
+    })
+
+    test("Single Element Snapshot", async ({ page }) => {
+        await page.goto("https://example.com")
+        const pageElement = await page.$("h1")
+        expect(await pageElement.screenshot()).toMatchSnapshot("page-title.png")        
     })
 })
