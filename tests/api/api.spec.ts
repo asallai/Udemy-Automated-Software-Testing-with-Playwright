89de2a26d897b30a16f2c4b884505acd3bae13ea
Aniko Sallai <aniko.sallai@gmail.com>
2023-11-24 22:37:17 +0100

    56. Simple API Test

---
+++ tests/api/api.spec.ts
@@ -0,0 +1,16 @@
+import { test, expect } from "@playwright/test"
+
+test.describe.parallel("API testing", () => {
+    const baseUrl = "https://reqres.in/api"
+
+    test("Simple API Test - Assert Response Status", async ({ request }) => {
+        const response = await request.get(`${baseUrl}/users/2`)
+        expect(response.status()).toBe(200)
+        
+    })
+
+    test("Simple API Test - Assert Invalid Endpoint", async ({ request }) => {
+        const response = await request.get(`${baseUrl}/users/non-existing-endpoint`)
+        expect(response.status()).toBe(404)        
+    })
+})
