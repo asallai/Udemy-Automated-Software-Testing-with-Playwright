20d39dc9d86984b6b236ffe573353f7db8e5de01
Aniko Sallai <aniko.sallai@gmail.com>
2023-11-25 19:47:54 +0100

    58. GET Request Test

--- tests/api/api.spec.ts
+++ tests/api/api.spec.ts
@@ -14,11 +14,24 @@ test.describe.parallel("API testing", () => {
         expect(response.status()).toBe(404)        
     })
 
-    test.only("Simple API test - Parse response JSON data", async ({ request }) => {
+    test("Simple API test - Parse response JSON data", async ({ request }) => {
         const response = await request.get(`${baseUrl}/users/3`)
         expect(response.status()).toBe(200)
 
         const responseBody = JSON.parse(await response.text())
         console.log(responseBody)        
+    })
+
+    test.only("GET Request - Get User Detail", async ({ request }) => {
+        const response = await request.get(`${baseUrl}/users/2`)
+        const responseBody = JSON.parse(await response.text())
+
+        console.log(responseBody)
+
+        expect(response.status()).toBe(200)
+        expect(responseBody.data.id).toBe(2)
+        expect(responseBody.data.first_name).toBe('Janet')
+        expect(responseBody.data.last_name).toBe('Weaver')
+        expect(responseBody.data.email).toBeTruthy        
     })
 })
