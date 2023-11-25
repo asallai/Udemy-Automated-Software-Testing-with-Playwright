36dfd6449dee94c52d8d48de725615cc1abde0ba
Aniko Sallai <aniko.sallai@gmail.com>
2023-11-25 20:18:06 +0100

    59. POST Request Test

--- tests/api/api.spec.ts
+++ tests/api/api.spec.ts
@@ -22,7 +22,7 @@ test.describe.parallel("API testing", () => {
         console.log(responseBody)        
     })
 
-    test.only("GET Request - Get User Detail", async ({ request }) => {
+    test("GET Request - Get User Detail", async ({ request }) => {
         const response = await request.get(`${baseUrl}/users/2`)
         const responseBody = JSON.parse(await response.text())
 
@@ -32,6 +32,49 @@ test.describe.parallel("API testing", () => {
         expect(responseBody.data.id).toBe(2)
         expect(responseBody.data.first_name).toBe('Janet')
         expect(responseBody.data.last_name).toBe('Weaver')
-        expect(responseBody.data.email).toBeTruthy        
+        expect(responseBody.data.email).toBeTruthy()   
+    })
+
+    test("POST Request - Create New User", async ({ request }) => {
+        const response = await request.post(`${baseUrl}/users`, {
+            data: {
+                name: 'Janet',
+                job: 'baker'
+            }
+        })
+
+        const responseBody = JSON.parse(await response.text())
+        console.log(responseBody)
+
+        expect(response.status()).toBe(201)        
+    })
+
+    test("POST Request - Login Successful", async ({ request }) => {
+        const response = await request.post(`${baseUrl}/login`, {
+            data: {
+                email: "eve.holt@reqres.in",
+                password: "cityslicka"
+            }
+        })
+
+        const responseBody = JSON.parse(await response.text())
+        console.log(responseBody)
+
+        expect(response.status()).toBe(200)
+        expect(responseBody.token).toBeTruthy()
+    })
+
+    test("POST Request - Login Unsuccessful", async ({ request }) => {
+        const response = await request.post(`${baseUrl}/login`, {
+            data: {
+                email: "eve.holt@reqres.in"
+            }
+        })
+
+        const responseBody = JSON.parse(await response.text())
+        console.log(responseBody)
+
+        expect(response.status()).toBe(400)  
+        expect(responseBody.error).toBe("Missing password")      
     })
 })
