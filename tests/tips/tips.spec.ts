4b04166ecde3b37e07d948290f569e0bc3f01917
Aniko Sallai <aniko.sallai@gmail.com>
2023-12-02 21:52:02 +0100

    68. Test Info Object

---
+++ tests/tips/tips.spec.ts
@@ -0,0 +1,9 @@
+import { test, expect } from "@playwright/test"
+
+test.describe.only("Tips & Tricks Section", () => {
+    test("TestInfo Object", async ({ page }, testInfo) => {
+        await page.goto("https://www.example.com")
+        console.log(testInfo) 
+        console.log(testInfo.expectedStatus)        
+    })    
+})
