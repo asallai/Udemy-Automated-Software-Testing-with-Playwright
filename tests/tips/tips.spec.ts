136c3fb3093acc3cf953b7cdf7592e12c5eedbae
Aniko Sallai <aniko.sallai@gmail.com>
2023-12-02 22:02:44 +0100

    69. Skip Browser Annotation

--- tests/tips/tips.spec.ts
+++ tests/tips/tips.spec.ts
@@ -1,9 +1,17 @@
 import { test, expect } from "@playwright/test"
 
-test.describe.only("Tips & Tricks Section", () => {
+test.describe("Tips & Tricks Section", () => {
+
     test("TestInfo Object", async ({ page }, testInfo) => {
         await page.goto("https://www.example.com")
         console.log(testInfo) 
         console.log(testInfo.expectedStatus)        
-    })    
+    })
+
+    test.only("Skip Browser Annotation",async ({ page, browserName }) => {
+        test.skip(browserName === "chromium", "Feature is not ready in Chrome browser")
+        await page.goto("https://www.example.com")
+        
+    })
+
 })
